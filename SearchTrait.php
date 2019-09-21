caf4a5f7a24b4ebe82f546db395beddddb92d896
黄磊 <huangl@golive.cc>
2019-09-21 19:46:52 +0800

    查询增加orWhere类型

--- SearchTrait.php
+++ SearchTrait.php
@@ -4,26 +4,25 @@ namespace app\common\traits;
 
 /**
  * 查询Trait
- *
  * Trait SearchTrait
+ * @package app\common\traits
  */
 trait SearchTrait
 {
     /**
      * 初始数组 默认为空数组
-     *
      * @var array
      */
     private $init = [];
 
     /**
      * 用户参数
-     *
      * @var
      */
     private $params;
 
     /**
+     * 获取用户参数
      * @param array $params
      * @return SearchTrait
      */
@@ -36,9 +35,7 @@ trait SearchTrait
 
     /**
      * 设置当前默认数组
-     *
      * @param $init array
-     *
      * @return SearchTrait
      */
     public function initial($init)
@@ -50,7 +47,6 @@ trait SearchTrait
 
     /**
      * 赋值
-     *
      * @param string $key 参数名
      * @param int $fuzzy 0精确查询,1模糊查询
      * @return SearchTrait
@@ -65,8 +61,23 @@ trait SearchTrait
     }
 
     /**
+     * orWhere查询
+     * @param string $key 参数名
+     * @param int $fuzzy 0精确查询,1模糊查询
+     * @param string $name 统一的参数名
+     * @return SearchTrait
+     */
+    public function orKey($key, $fuzzy = 0, $name = '')
+    {
+        if (isset($this->params[$name]) && !empty($this->params[$name])) {
+            $this->init[$key] = $fuzzy ? ['LIKE', '%' . $this->params[$name] . '%'] : $this->params[$name];
+        }
+
+        return $this;
+    }
+
+    /**
      * 获取结果
-     *
      * @return array
      */
     public function result()
@@ -74,4 +85,3 @@ trait SearchTrait
         return $this->init;
     }
 }
-
