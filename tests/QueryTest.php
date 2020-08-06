d7cf6bb0820dd66f34724bee77f63c8e606b809e
Mitirrli <huanglei@quanjingshuju.com>
2020-08-06 17:22:48 +0800

    add test case

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -73,4 +73,63 @@ class QueryTest extends TestCase
         self::assertEquals($test2[0], 'LIKE');
         self::assertEquals($test2[1], '%' . TestData::TEST_DATA2[$key] . '%');
     }
+
+    /**
+     * test function orKey.
+     */
+    public function testOrKey()
+    {
+        $key = 'test';
+        $new_key = 'new key';
+
+        $result = $this->param(TestData::TEST_DATA2)->orKey($new_key, $key)->result();
+
+        self::assertIsArray($result);
+        self::assertArrayHasKey($new_key, $result);
+
+        //Test 1. accurate search
+        $key = 'JS';
+        $new_key = 'NEW_JS';
+        $object = $this->param(TestData::TEST_DATA3)->orKey($new_key, $key);
+
+        $property = new \ReflectionProperty($object, 'init');
+        $property->setAccessible(true);
+        self::assertEquals(TestData::TEST_DATA3[$key], $property->getValue($object)[$new_key]);
+
+        //Test 2. right fuzzy search
+        $key = 'PHP';
+        $new_key = 'NEW_PHP';
+        $object = $this->param(TestData::TEST_DATA3)->orKey($new_key, $key, Search::PERCENT_RIGHT);
+
+        $property = new \ReflectionProperty($object, 'init');
+        $property->setAccessible(true);
+
+        self::assertIsArray($test2 = $property->getValue($object)[$new_key]);
+        self::assertEquals($test2[0], 'LIKE');
+        self::assertEquals($test2[1], TestData::TEST_DATA3[$key] . '%');
+
+        //Test 2. right fuzzy search
+        $key = 'C';
+        $new_key = 'NEW_C';
+        $object = $this->param(TestData::TEST_DATA3)->orKey($new_key, $key, Search::PERCENT_ALL);
+
+        $property = new \ReflectionProperty($object, 'init');
+        $property->setAccessible(true);
+
+        self::assertIsArray($test2 = $property->getValue($object)[$new_key]);
+        self::assertEquals($test2[0], 'LIKE');
+        self::assertEquals($test2[1], '%' . TestData::TEST_DATA3[$key] . '%');
+    }
+
+    /**
+     * test function result.
+     */
+    public function testResult()
+    {
+        $keys = array_keys(TestData::TEST_DATA2);
+
+        array_map(function ($value) {
+            self::assertArrayHasKey($value, $this->param(TestData::TEST_DATA2)->key($value)->result());
+        }, $keys);
+    }
 }
